e0039b6df64aa9e62814b4b22602dcc1f654d5a2
fmarp <fmarp@D-00888141.bndes.net>
2017-07-25 15:46:07 -0300

    Revert "Acrescentado 2 números e média"
    This reverts commit 9c87762c36465df0814fc519c90abe7a84f95036.

--- ProjetoTeste/src/ClasseTeste.java
+++ ProjetoTeste/src/ClasseTeste.java
@@ -9,32 +9,17 @@ public class ClasseTeste {
 		ler = new Scanner(System.in);
 		
 		String palavra;
-		int qtd, num=0, numtotal=0;
-		float avg;
+		int i;
 		
 		
 		System.out.println("Escreva uma frase:");
 		palavra = ler.nextLine();
 		
-		System.out.printf("%s\n", palavra);
+		System.out.println("Escreva um numero:");
+		i = ler.nextInt();
 		
 		
-		
-		System.out.println("Escreva a quantidade de entradas:");
-		qtd = ler.nextInt();
-		
-		for(int i = 0; i < qtd ; i++) {
-			System.out.printf("Digite o numero %d: \n", i+1);
-			num = ler.nextInt();
-			numtotal = numtotal + num;
-		}
-		
-		avg = numtotal/qtd;
-		
-		System.out.printf("A media é: %f", avg);
-		
-		
-		
+		System.out.printf("%s %d\n", palavra, i);
 	}
 
 }
